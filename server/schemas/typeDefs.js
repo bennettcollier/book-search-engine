63a20f9be4f9c867a9d32ca2b5622348d35f5f8e
Bennett Collier <bennettcollier@Bennetts-MacBook-Pro.local>
2021-03-28 22:46:14 -0700

    final commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount:Int 
+    savedBooks:[Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: ID!
+    authors:[String]
+    description: String
+    title:String!
+    image:String
+    link: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  input BookInput {
+    authors:[String]
+    description: String
+    title: String! 
+    image:String
+    link:String
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      bookId:ID, 
+      authors:[String],
+      description: String,
+      title: String, 
+      image:String,
+      link:String
+    ): User
+    deleteBook(bookId:ID):User
+  }
+`;
+
+module.exports = typeDefs;
